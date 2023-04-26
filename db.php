ef33e2448c6ff3025756513db152d0177e651435
mdaaamax <omgwtfmda@mail.ru>
2023-04-27 03:01:59 +0400

    1

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 const DB_PROVIDER = 'pgsql';
 const DB_HOST = 'localhost';
-const DB_BASENAME = 'lesson_forum';
+const DB_BASENAME = 'beermax';
 const DB_USERNAME = 'postgres';
 const DB_PASSWORD = '';
 
